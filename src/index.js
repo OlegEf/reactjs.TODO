42cffa221d509076c4fca78191109c8aba19eae4
OlegEf <5tg6yy@gmail.com>
2020-12-22 17:08:52 +0300

    react element

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 // import './index.css';
 // import App from './App';
 // import reportWebVitals from './reportWebVitals';
+const el=<h1>react element jsx</h1>;
+// const el=React.createElement('h1',null,'Hello World!');
+// const el=React.createElement('h1',null,'react element');
 
-ReactDOM.render(
-  // <React.StrictMode>
-    <App />
-  // </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(el,document.getElementById('root'));
+
+// ReactDOM.render(<App/>,document.getElementById('root')
+// );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
