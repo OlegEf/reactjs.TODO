be235ab3ef85439f0b79587dae4788c6d8e6d510
OlegEf <5tg6yy@gmail.com>
2020-12-31 00:10:12 +0300

    react key

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -3,16 +3,19 @@ import TodoListItem from './todo-list-item';
 
 const TodoList = ({todos}) => {
 	const elements = todos.map((item) => {
+
+	const { id, ...itemProps } = item;
+
 		return (
-			<li>
-				<TodoListItem{...item}/>
+			<li key={id}>
+				<TodoListItem{...itemProps}/>
 			</li>
 		);
 	});
 	
 	return (
 		<ul className="list-group-item">
-			{elements}
+			{ elements }
 		</ul>
 	);
 };
