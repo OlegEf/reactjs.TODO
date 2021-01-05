6b644aa568e45edd81e2174260b5b93674419b11
OlegEf <5tg6yy@gmail.com>
2021-01-05 18:19:50 +0300

    add folders in components refactoring

--- src/components/app-header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const AppHeader = () => {
-	return (
-		<h1 className="list-group-item">my TodoList</h1>
-	);
-};
-
-export default AppHeader;
--- src/index.js
+++ src/index.js
@@ -1,30 +1,7 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import AppHeader from './components/app-header';
-import SearchPannel from './components/search-panel';
-import TodoList from './components/todo-list';
-// import TodoListItem from './components/todo-list-item';
+import App from './components/app';
 
-const App = () => {
-
-	const todoData=[
-		{label: 'Drink Coffee', important: false},
-		{label: 'Make Awesome App', important: true},
-		{label: 'have a lunch' , important: false}
-	];
-
-	return (
-		<div>
-			<AppHeader/>
-			<SearchPannel/>
-			<TodoList todos={todoData}/>
-		</div>
-	);
-};
-
-
-	
-ReactDOM.render(<App/>,document.getElementById('root'));
-
+ReactDOM.render(<App />,
+  document.getElementById('root'));
