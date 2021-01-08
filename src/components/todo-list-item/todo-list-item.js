6194cc759e68d01977de1e6ad2f5ed18a6e9ece4
OlegEf <5tg6yy@gmail.com>
2021-01-09 02:46:40 +0300

    str.7, 22, 33 react todo-list-item add state

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -4,19 +4,33 @@ import './todo-list-item.css';
 
 export default class TodoListItem extends Component{
 
+	state = {
+		done: false
+	}
+
 	onLabelClick = () => {
-		console.log(`Done: ${this.props.label}`);
+		this.setState({
+			done: true
+		});
 	};
+		// console.log(`Done: ${this.props.label}`);
 
 	render(){
-		const { label, important=false } = this.props;
+	  const { label, important=false } = this.props;
+	  const { done } = this.state;
+
+	  let classNames = 'todo-list-item';
+	  if (done) {
+		  classNames += ' done ';
+	  }
+
 	  const style = {
 		color: important ? 'steelblue' : 'black',
 		fontWeight: important ? 'bold' : 'normal'
 	  };
 		
 	  return (
-		<span className="todo-list-item">
+	<span className={classNames}>
 		  <span
 			className="todo-list-item-label"
 			style={style} onClick={ this.onLabelClick } >
