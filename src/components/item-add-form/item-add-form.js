23fc28793ba63583f56a3515ef64104ec23e067b
OlegEf <5tg6yy@gmail.com>
2021-01-16 21:19:32 +0300

    item-add-form.js str 17 add onSubmit str.27 add form str.35 dell onClick hw

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -14,20 +14,28 @@ export default class ItemAddForm extends Component {
     });
   };
 
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onItemAdded(this.state.label);
+    this.setState({
+      label: ''
+    });
+  };
+
   render() {
     return (
-      <div className="item-add-form">
+      <form className="item-add-form d-flex"
+		onSubmit={this.onSubmit}>
 		<input type="text"
                className="form-control"
                onChange={this.onLabelChange}
                placeholder="What needs to be done"
 			   />
         <button
-          className="btn btn-outline-secondary"
-          onClick={() => this.props.onItemAdded('Hello World')}>
+          className="btn btn-outline-secondary">
           Add Item
         </button>
-      </div>
+      </form>
     )
   }
 }
